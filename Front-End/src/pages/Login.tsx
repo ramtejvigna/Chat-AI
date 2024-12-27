c4656a0da0a188e902d8de215cceb5bbc810f235
ramtejvigna <vignaramtejtelagarapu@gmail.com>
2024-12-27 23:12:04 +0530

    Changed image

--- Front-End/src/pages/Login.tsx
+++ Front-End/src/pages/Login.tsx
@@ -30,7 +30,7 @@ const Login = () => {
   return (
     <Box width={"100%"} height={"100%"} display="flex" flex={1}>
       <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
-        <img src="airobot.png" alt="Robot" style={{ width: "400px" }} />
+        <img src="robot.png" alt="Robot" style={{ width: "400px" }} />
       </Box>
       <Box
         display={"flex"}
